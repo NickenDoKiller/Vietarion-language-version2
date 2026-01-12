e823ed768c57655bfa9c2b0192ed35b2050e7427
NickenDoKiller <nickendokiller@gmail.com>
2026-01-12 15:10:06 +0700

    Delete vietarion/target directory

--- vietarion/target/debug/deps/vietarion-6f783671ec17b717.d
+++
@@ -1,8 +0,0 @@
-/home/nickendokiller/vietarion/target/debug/deps/vietarion-6f783671ec17b717.d: src/main.rs src/lexer.rs src/token.rs src/error.rs
-
-/home/nickendokiller/vietarion/target/debug/deps/libvietarion-6f783671ec17b717.rmeta: src/main.rs src/lexer.rs src/token.rs src/error.rs
-
-src/main.rs:
-src/lexer.rs:
-src/token.rs:
-src/error.rs:
